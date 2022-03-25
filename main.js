42913b2730bc2ce70b22548c2573934a588e3e54
ADMIN\Admin <vuong1st.fb@gmail.com>
2022-03-25 18:37:46 +0700

    add project

---
+++ main.js
@@ -0,0 +1,37 @@
+const slider = document.querySelector(".parent");
+let mouseDown = false;
+let startX, scrollLeft;
+
+let startDragging = function (e) {
+  mouseDown = true;
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+};
+let stopDragging = function (event) {
+  mouseDown = false;
+};
+
+slider.addEventListener("mousemove", (e) => {
+  e.preventDefault();
+  if (!mouseDown) {
+    return;
+  }
+  const x = e.pageX - slider.offsetLeft;
+  const scroll = x - startX;
+  slider.scrollLeft = scrollLeft - scroll;
+});
+
+// Add the event listeners
+slider.addEventListener("mousedown", startDragging, false);
+slider.addEventListener("mouseup", stopDragging, false);
+slider.addEventListener("mouseleave", stopDragging, false);
+
+function toggleNavigation(params) {
+  let x = document.getElementById(params);
+  x.classList.toggle("hidden");
+}
+
+function closeSearch(){
+  let x = document.getElementById('search-mobile');
+  x.classList.add("hidden");
+}
